d51f7631e435968f891268b4a7cb45e61c53282f
FelipeBertol <felipe.bertoli@edu.unifil.br>
2023-09-18 17:04:06 -0300

    Texto dinâmico

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  textGrade:string = "Graduando em Ciência da Computação"
+  textRole:string = "Aspirante a Desenvolvedor Web"
+  textWelcome:string = "Bem vindo! Meu nome é "
+  roles:string[] = ['Web', 'Java']
+  itemAtual:string = '';
+  indiceAtual:number = 0;
+  cssClass:string = '';
 
+  ngOnInit(): void {
+    this.atualizarValor();
+  }
+
+  atualizarValor() {
+    this.cssClass = 'welcome-subtitle span';
+    this.indiceAtual = (this.indiceAtual + 1) % this.roles.length;
+    this.itemAtual = this.roles[this.indiceAtual];
+    setTimeout(() => {
+      this.atualizarValor();
+    }, 7000); 
+    
+
+  }
 }
