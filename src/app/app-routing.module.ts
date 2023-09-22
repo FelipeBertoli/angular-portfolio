92b72bd82426e096ebfe0086ed926b4ef000de7c
FelipeBertol <felipe.bertoli@edu.unifil.br>
2023-09-22 13:06:05 -0300

    Componenete dinâmicos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ProjectsComponent } from './pages/projects/projects.component';
 
 const routes: Routes = [
   {
-    path: 'home', 
+    path: '', 
     component: HomeComponent
   }, 
   {
--- src/app/components/about-text/about-text.component.ts
+++ src/app/components/about-text/about-text.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-about-text',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-text.component.css']
 })
 export class AboutTextComponent {
+  @Input() aboutText:string = 'Atualmente cursando Ciência da Computação na UniFil de Londrina, estou em busca de aprimorar minhas habilidades na área, especialmente no que se diz respeito ao desenvolvimento full-stack. Atuo na Tata Consultancy Services, onde desenvolvo APIs, páginas e serviços utilizando no-code.'
   imagePath:string = 'https://avatars.githubusercontent.com/u/103579021?s=400&u=2b5706ebceca457e77277be91de0873b477370a8&v=4'
 }
