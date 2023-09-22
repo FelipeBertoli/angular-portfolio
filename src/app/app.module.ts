f3acbc7ee0739b203acb81a7e918693f2733e217
FelipeBertol <felipe.bertoli@edu.unifil.br>
2023-09-22 13:27:52 -0300

    Excluindo arquivos modificados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { AboutTextComponent } from './components/about-text/about-text.component';
-import { AboutSkillsComponent } from './components/about-skills/about-skills.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
-import { GamesComponent } from './components/projects/games/games.component';
-import { SitesComponent } from './components/projects/sites/sites.component';
+import { GamesComponent } from './pages/projects/games/games.component';
+import { SitesComponent } from './pages/projects/sites/sites.component';
+import { DetailsComponent } from './pages/about/details/details.component';
+import { SkillsComponent } from './pages/about/skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +18,11 @@ import { SitesComponent } from './components/projects/sites/sites.component';
     MenuBarComponent,
     HomeComponent,
     AboutComponent,
-    AboutTextComponent,
-    AboutSkillsComponent,
     ProjectsComponent,
     GamesComponent,
-    SitesComponent
+    SitesComponent,
+    DetailsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
